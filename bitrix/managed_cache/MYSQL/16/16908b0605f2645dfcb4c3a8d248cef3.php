7da6b5e7e1a64493ba37762a4fc311914f0eb3da
AndreyPantyukhin <82023358+AndreyPantyukhin@users.noreply.github.com>
2025-01-13 07:25:50 +0700

    index.html deleted

--- bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
+++ bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001736626478';
-$dateexpire = '001736630078';
+$datecreate = '001736658465';
+$dateexpire = '001736662065';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:1;}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/b_option/e6/e69b462580b3cd8550c28eb99ac27172.php
+++ bitrix/managed_cache/MYSQL/b_option/e6/e69b462580b3cd8550c28eb99ac27172.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001736625653';
-$dateexpire = '001736629253';
+$datecreate = '001736658358';
+$dateexpire = '001736661958';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:2:{s:20:"ipcheck_disable_file";s:64:"/bitrix/modules/ipcheck_disable_fe83b2234fd61d6d17936d26f4783d90";s:12:"redirect_sid";s:32:"3x9nruq68ee8c4j7erg5nar4q8pfm602";}}}';
 return true;
 ?>
